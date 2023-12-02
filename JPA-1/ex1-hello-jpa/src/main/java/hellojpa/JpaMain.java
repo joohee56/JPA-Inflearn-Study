8281c1fc1836797d1d882cd131ed1d0911b42f4b
Joohui <joohuii96@gmail.com>
2023-12-02 19:54:13 +0900

    Section 1-2. jpa 기초 실습

---
+++ JPA-1/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,29 @@
+package hellojpa;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
