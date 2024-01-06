5e157a52389651d44c6c3d1cb956bf1d55c32225
Joohui <joohuii96@gmail.com>
2024-01-06 11:05:50 +0900

    feat: 주문 조히 V2-엔티티를 DTO로 변환(fetch joinX)

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -28,6 +29,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;  //주문 상품
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;    //주문
